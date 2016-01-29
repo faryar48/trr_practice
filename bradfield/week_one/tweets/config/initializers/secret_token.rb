032a80800b9c3243261327052ba1bde746a31963
Faryar Ghaemi <faryarg88@gmail.com>
2016-01-28 16:27:39 -0800

    new app

---
+++ bradfield/week_one/tweets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweets::Application.config.secret_token = 'a67f91324cff7c1aa364c3ceae15958eab87002125b30b5d0ec12ac049de293f2ba31071a4d3dfa046eb7e46d9ee9c3da723bab526d4d7f80a996f25647f05a9'
